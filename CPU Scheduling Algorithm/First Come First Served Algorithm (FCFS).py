e1d1bf37107d2eafa721049deea57eb6ef4f5748
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2021-09-27 23:30:54 +0530

    changes in program and added comments

--- CPU Scheduling Algorithm/First Come First Served Algorithm (FCFS).py
+++ CPU Scheduling Algorithm/First Come First Served Algorithm (FCFS).py
@@ -1,33 +1,37 @@
 # FCFS Algorithm in Python
 
-# This is the function: It takes input parameter process
-def FCFS(process):
+# This is the function: It takes input parameter process, and number of process
+def FCFS(process, n):
     wait, time = 0,0                # Initailzing some variables that will be used in function
-    waitsum, turnsum = 0,0          # Waitsum -> Sum of Waiting Time
-                                    # Turnsum -> Sum of Turnaround Time
-    space = 2*" "                   # Used for printing values
+    waitSum, turnSum = 0,0          # Initializing Sum of Waiting-Time
+                                    # Initializing Sum of Turnaround Time
     
 
-    print("Process Burst-Time\tWaiting-Time\tTurnaround-Time")
+    print("Process\t    Burst-Time\tWaiting-Time\tTurnaround-Time")
     for i in range(n):                                  # Iterating through all process
-        turn = wait + process[i][1]                     # Turnaround Time  = Waiting Time + Burst Time of Process
-        print("P" + str(process[i][0]), space, process[i][1], space, wait, space, turn)
-        wait += process[i][1]                           # Incrementing waiting time with Burst Time of current process
-        waitsum += wait
-        turnsum += turn
+        turn = wait + process[i][1]                     # Turnaround-Time  = Waiting-Time + Burst-Time of Process
+        
+        print("P" + str(process[i][0]) + "\t\t" + str(process[i][1]) + "\t\t" + str(wait)+ "\t\t" + str(turn))
+        wait += process[i][1]                           # Incrementing waiting-time with Burst-Time of current process
+        waitSum += wait                                 # Incrementing Sum of Wait-Time
+        turnSum += turn                                 # Incrementing Sum of Wait-Time
 
-    waitsum -= wait
-    print("\nAverage Waiting-Time is", round(waitsum/n, 3))
-    print("Average Turnaround-Time is", round(turnsum/n, 3))
+    waitSum -= wait
+    avgWaitTime = round(waitSum/n, 3)                       # Rounding Average Waiting Time upto 3 decimal places
+    avgTurnTime = round(turnSum/n, 3)                       # Rounding Average Turnaround-Time upto 3 decimal places
+    
+    return (avgWaitTime, avgTurnTime)
     
 
 # Driver Code
 if __name__ == "__main__":
-    n=int(input("Enter the number of process: "))
-    process=[]                                  # Process is list for all the processes to be executed 
+    n=int(input("Enter the number of process: "))           # Number of processes
+    process=[]                                              # Process is list for all the processes to be executed 
 
     for i in range(1, n+1):
         process.append([i, int(input("Enter Burst-Time for Process-"+str(i)+": "))])
 
     print("\n\nFirst Come First Served Scheduling: ")
-    FCFS(process)
+    resultTuple = FCFS(process, n)
+    print("\nAverage Waiting-Time is", resultTuple[0])
+    print("Average Turnaround-Time is", resultTuple[1])

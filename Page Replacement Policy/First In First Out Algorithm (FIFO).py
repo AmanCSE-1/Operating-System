0ca82e37e1f8bfb08390e97ce290caab341ac28d
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2021-09-30 23:12:36 +0530

    Added comments

--- Page Replacement Policy/First In First Out Algorithm (FIFO).py
+++ Page Replacement Policy/First In First Out Algorithm (FIFO).py
@@ -8,7 +8,7 @@ def FIFO(pages, capacity):
     for page in pages:
         if (page not in memory):                        # If page not found in memory
             pageFaults+=1                               # Incrementing the count of Page faults
-            if (len(memory) < capacity):                # Free spaces in frames
+            if (len(memory) < capacity):                # If Free spaces available in frames
                 memory.append(page)                     # Add page to memory if free space exists
                 
             else:                                       # If page not found in memory, apply FIFO Page Replacement Algorithm
@@ -20,7 +20,7 @@ def FIFO(pages, capacity):
 # Driver Code to run the program  
 if __name__ == "__main__":
     pages = list(map(int, input('Enter the sequence of Pages: ').strip().split()))
-    capacity=int(input('Enter maximum number of pages in a frame: '))                 # max pages in frame
+    capacity = int(input('Enter maximum number of pages in a frame: '))                 # max pages in frame
 
     print("\nThe number of Page Faults occurred in FIFO are: ", FIFO(pages, capacity))
     

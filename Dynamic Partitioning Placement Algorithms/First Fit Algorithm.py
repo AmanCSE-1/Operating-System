4deb1dc5ac184ce0cc777dbb77b70aa02e43e2f0
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2021-10-03 23:42:05 +0530

    Create First Fit Algorithm.py

---
+++ Dynamic Partitioning Placement Algorithms/First Fit Algorithm.py
@@ -0,0 +1,18 @@
+def FirstFit(blockSize, processSize):
+    allocate = [-1]*len(blockSize)
+        for i in range(len(processSize)):
+            for j in range(len(blockSize)):
+                if blockSize[j] >= processSize[i]:
+                    allocate[i] = j
+                    blockSize[j] -= processSize[i]
+        break
+    
+    
+    print(" Process No\t Process Size \t Block Number")
+    
+    for i in range(n):
+        print(" ", i + 1, "\t\t", processSize[i], "\t\t", end = " ")
+        if allocate[i] != -1:
+            print(allocate[i] + 1)
+        else:
+            print ("Not Allocated")

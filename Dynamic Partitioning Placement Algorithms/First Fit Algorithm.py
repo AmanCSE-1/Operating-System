6f12551fa212b2105a52374da333efce2f3239c6
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2021-10-03 23:47:00 +0530

    Added comments

--- Dynamic Partitioning Placement Algorithms/First Fit Algorithm.py
+++ Dynamic Partitioning Placement Algorithms/First Fit Algorithm.py
@@ -1,18 +1,18 @@
 # First Fit Dynamic Partitioning Placement Algorithm in Python
 
 def FirstFit(blockSize, processSize):
-    allocate = [-1]*len(blockSize)
+    allocate = [-1]*len(blockSize)                          # Initializing allocate list 
     
-        for i in range(len(processSize)):
+        for i in range(len(processSize)):                   #
             for j in range(len(blockSize)):
-                if blockSize[j] >= processSize[i]:
+                if blockSize[j] >= processSize[i]:          # if block size is greater than or equal to process size, then allocate the process
                     allocate[i] = j
                     blockSize[j] -= processSize[i]
         break
     
     
+    # Display the processes with the blocks that are allocated to a respective process
     print(" Process No\t Process Size \t Block Number")
-    
     for i in range(n):
         print(" ", i + 1, "\t\t", processSize[i], "\t\t", end = " ")
         if allocate[i] != -1:
@@ -20,3 +20,14 @@ def FirstFit(blockSize, processSize):
             
         else:
             print ("Not Allocated")
+
+            
+# Driver code to run the Program
+if __name__ == "__main__":
+    
+    # get the size of each block and process requests.
+    blockSize = list(map(int, input("Enter the size of each block : ").strip().split()))        
+    processSize = list(map(int, input("Enter the size of each process : ").strip().split()))
+    
+    # Call the function
+    FirstFit(blockSize, processSize)

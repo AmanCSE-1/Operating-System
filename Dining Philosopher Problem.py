cc28ae25bf588e847a389cce1ac681b860654875
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2021-10-11 23:09:04 +0530

    Initial Commit
    Added constructor and attributes

---
+++ Dining Philosopher Problem.py
@@ -0,0 +1,11 @@
+import threading
+import time
+
+class Philosopher(threading.Thread):
+    running = True
+    
+    def __init__(self, index, forkOnLeft, forkOnRight):
+        threading.Thread.__init__(self)
+        self.index = index
+        self.forkOnLeft = forkOnLeft
+        self.forkOnRight = forkOnRight

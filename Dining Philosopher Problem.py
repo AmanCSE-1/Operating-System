c196631d36e22a799a25831373d691e715b95bc6
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2021-10-11 23:09:47 +0530

    Update Dining Philosopher Problem.py
    Added methods run and dine

--- Dining Philosopher Problem.py
+++ Dining Philosopher Problem.py
@@ -9,3 +9,26 @@ class Philosopher(threading.Thread):
         self.index = index
         self.forkOnLeft = forkOnLeft
         self.forkOnRight = forkOnRight
+        
+    def run(self):
+        while(self.running):
+            time.sleep(5)
+            print('Philosopher %s is hungry.' %self.index)
+            self.dine()
+    
+    def dine(self):
+        fork1, fork2 = self.forkOnLeft, self.forkOnRight
+        while(self.running):
+            fork1.acquire()
+            locked = fork2.acquire(False)
+            if locked: break
+                
+            fork1.release()
+            print('Philosopher %s swaps forks.' %self.index)
+            fork1, fork2 = fork2, fork1
+        
+        else:
+            return
+        self.dining()
+        fork2.release()
+        fork1.release()
